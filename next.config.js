76639432096421709a50f6b1b0d9fa6dbe667e0c
christianhacevedo-cmyk <christianhacevedo@gmail.com>
2026-04-24 23:33:20 -0500

    disable SWC minify and source maps

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  swcMinify: false,
+  productionBrowserSourceMaps: false,
+  // Disable build traces which is causing stack overflow
+  experimental: {
+    optimizePackageImports: ['lodash'],
+  },
 };
 
 module.exports = nextConfig;
